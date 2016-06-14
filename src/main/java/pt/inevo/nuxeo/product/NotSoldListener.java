af3e1273d0573504fc82d725ff0d6e4eeca2908f
miguel <nyxu15@hotmail.com>
2016-06-14 15:29:24 +0100

    Added NotSoldEvent, listener and respective (basic) handling.

---
+++ src/main/java/pt/inevo/nuxeo/product/NotSoldListener.java
@@ -0,0 +1,41 @@
+package pt.inevo.nuxeo.product;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.nuxeo.ecm.collections.api.CollectionConstants;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.core.event.Event;
+import org.nuxeo.ecm.core.event.EventContext;
+import org.nuxeo.ecm.core.event.EventListener;
+import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
+
+public class NotSoldListener implements EventListener {
+
+	@Override
+	public void handleEvent(Event event) {
+		
+		EventContext ctx = event.getContext();
+        if (!(ctx instanceof DocumentEventContext)) {
+            return;
+        }
+        
+        DocumentModel doc = ((DocumentEventContext) ctx).getSourceDocument();
+        if (doc == null) {
+            return;
+        }
+        
+        @SuppressWarnings("unchecked")
+		List<String> visualIds = (List<String>) doc.getPropertyValue(CollectionConstants.DOCUMENT_COLLECTION_IDS_PROPERTY_NAME);
+        for (String visualId: visualIds) {
+        	IdRef docIdRef = new IdRef(visualId);
+        	DocumentModel visual = ctx.getCoreSession().getDocument(docIdRef);
+        	Assert.assertNotNull(visual);
+        }
+        
+        System.out.println("Handled event in NotSoldListener.");
+		
+	}
+
+}
---
+++ src/test/java/pt/inevo/nuxeo/product/event/TestNotSoldEvent.java
@@ -0,0 +1,61 @@
+package pt.inevo.nuxeo.product.event;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.core.event.EventProducer;
+import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
+import org.nuxeo.ecm.core.test.CoreFeature;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.ecm.core.event.Event;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.LocalDeploy;
+
+import com.google.inject.Inject;
+
+@RunWith(FeaturesRunner.class)
+@Features({ CoreFeature.class })
+@Deploy("studio.extensions.rdias-SANDBOX")
+@LocalDeploy("nuxeo-product-service:OSGI-INF/listeners/product-not-sold-listener.xml")
+public class TestNotSoldEvent {
+
+    @Inject
+    protected CoreSession coreSession;
+    
+    @Test
+    public void testNotSoldEvent() {
+   
+    	DocumentModel doc = coreSession.createDocumentModel("/", "Test Product", "Product");
+    	doc.setProperty("nxproduct","productPrice", "100");
+    	doc = coreSession.createDocument(doc);
+    	coreSession.save();
+    	
+    	IdRef docIdRef = new IdRef(doc.getId());
+    	doc = coreSession.getDocument(docIdRef);
+    	Assert.assertNotNull(doc);
+    	
+		EventProducer eventProducer;
+		try {
+			eventProducer = Framework.getService(EventProducer.class);
+		} catch (Exception e) {
+			return;
+		}
+		DocumentEventContext ctx = new DocumentEventContext(coreSession, coreSession.getPrincipal(), doc);
+		Event event = ctx.newEvent("notSoldEvent");
+		eventProducer.fireEvent(event);
+    	
+    }
+	
+}
